a1d312e85f942ac3cad86ad018dd5acdbcb016e5
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-12-13 13:43:29 +0100

    Generare 10 email e stamparli nel doom attraverso l'utilizzo dell'apposita API

--- script.js
+++ script.js
@@ -8,13 +8,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            mailList: []
         }
     },
     methods: {
 
     },
     mounted() {
-
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((risposta) => {
+                    console.log(risposta.data.response);
+                    this.mailList.push(risposta.data.response);
+                });
+        }
     }
 }).mount("#contenitore");
